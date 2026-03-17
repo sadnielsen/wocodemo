875e90189fdcfbd0cd1e353ac04766840a000103
Svend <svend@sn71.eu>
2026-03-17 10:42:43 +0100

    Added DI into WPF.

--- src/WoCo.Wpf/App.xaml.cs
+++ src/WoCo.Wpf/App.xaml.cs
@@ -1,6 +1,12 @@
-﻿using System.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using WoCo.Core.DataAccess;
+using WoCo.Core.DependencyInjection;
 
 namespace WoCo.Wpf;
 
@@ -9,5 +15,38 @@ namespace WoCo.Wpf;
 /// </summary>
 public partial class App : Application
 {
+    private IHost? _host;
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        _host = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) =>
+            {
+                // Services
+                services.ConfigureWoCoServices();
+
+                // Views
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
+
+        await _host.StartAsync();
+
+        var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        if (_host is not null)
+        {
+            _host.StopAsync().GetAwaiter().GetResult();
+            _host.Dispose();
+        }
+
+        base.OnExit(e);
+    }
 }
 
